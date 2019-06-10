6b7dd6d3f0295809e56b19d9ea4c8b5289f7a355
Michael Aaron Murphy <mmstick@pm.me>
2019-06-10 17:12:43 -0600

    feat(disk-prober): Integrate disk-prober with ecs-disk-manager
    Now that the disk prober's functionality is almost complete, it is time
    to integrate it into our ECS so that we can try it out in practice. As
    soon as we've finished integrating basic disk probing with the ECS, we
    should look into improving the support for LVM logical volumes and their
    volume groups.
    
    It seems to be integrating really well -- very little effort was
    required to interface with the ECS. The ECS is able to provide
    assocations in both ways for a device: associating parent devices with
    child(ren) and children with their parent device(s).
    
    Note that at this time the ECS is only available to gather information. I
    haven't gotten to implementing the S in ECS yet. As soon as LVM and LUKS
    device information gathering is complete, I can begin working on
    integrating partition table management support.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,11 @@
-#[macro_use]
-extern crate auto_enums;
-
 mod builder;
 
+use disk_prober::{slaves_iter, DeviceVariant, DiskProberError, Prober};
 use disk_types::prelude::*;
 use slotmap::*;
 use std::{
     collections::{HashMap, HashSet},
-    iter,
+    path::Path,
     sync::{
         atomic::{AtomicBool, Ordering},
         Arc,
@@ -20,11 +18,13 @@ pub use slotmap::DefaultKey as Entity;
 #[derive(Debug)]
 pub enum DiskError {
     Cancelled,
+    Prober(DiskProberError),
+    UnknownFS(Box<str>),
 }
 
 #[derive(Debug, Default)]
 pub struct DiskManager {
-    entities: SlotMap<Entity, ()>,
+    entities: HopSlotMap<Entity, ()>,
     // Components representing current data on devices.
     storage: DiskComponents,
     // Operations that will be carried out when systems are invoked
@@ -33,30 +33,31 @@ pub struct DiskManager {
 
 #[derive(Debug, Default)]
 struct DiskComponents {
-    // Devices that have children will associate their children here.
-    pub children: SparseSecondaryMap<Entity, Entity>,
+    // Devices that contain children will associate their children here.
+    pub children: SparseSecondaryMap<Entity, Vec<Entity>>,
     // Every entity in the world has a device, so accesses to this should be infallable
     pub devices: SparseSecondaryMap<Entity, Device>,
     // If a device represents a physical disk,its information is here.
     pub disks: SparseSecondaryMap<Entity, Disk>,
+    // Device maps that were discovered in the system.
+    pub device_maps: SparseSecondaryMap<Entity, Box<str>>,
+    // Loopback devices will have a backing file associated with them.
+    pub loopbacks: SparseSecondaryMap<Entity, Box<Path>>,
     // If a device is a LUKS device, its information is here.
     pub luks: SparseSecondaryMap<Entity, Luks>,
     // If a device is a LVM group, its information is here.
     pub lvms: SparseSecondaryMap<Entity, Lvm>,
     // If the device has a parent, it will be associated here.
-    pub parents: SparseSecondaryMap<Entity, Entity>,
-    // Devices that are partitions can fetch their partition information here.
+    pub parents: SparseSecondaryMap<Entity, Vec<Entity>>,
+    // Devices with parent(s) will associate their parent device(s) here.
     pub partitions: SparseSecondaryMap<Entity, Partition>,
-    // Devices with tables will associate their tables here.
-    pub tables: SparseSecondaryMap<Entity, Vec<Entity>>,
 }
 
 impl DiskManager {
     /// Reloads all disk information from the system.
     pub fn scan(&mut self) -> Result<(), DiskError> {
         self.entities.clear();
-
-        unimplemented!();
+        self.scan_()
     }
 
     pub fn add(&mut self, device: Entity, info: PartitionBuilder) -> Result<(), DiskError> {
@@ -133,6 +134,116 @@ impl DiskManager {
 
         Ok(())
     }
+
+    #[cfg(target_os = "linux")]
+    fn scan_(&mut self) -> Result<(), DiskError> {
+        let prober = Prober::new().map_err(DiskError::Prober)?;
+        for res in prober.into_iter().filter_map(Result::transpose) {
+            let probed = res.map_err(DiskError::Prober)?;
+            let info = probed.probe().map_err(DiskError::Prober)?;
+
+            let whole_entity = self.entities.insert(());
+
+            self.storage.devices.insert(
+                whole_entity,
+                Device {
+                    name:                 Box::from(info.device),
+                    path:                 Box::from(info.path),
+                    sectors:              info.sectors,
+                    logical_sector_size:  info.logical_sector_size,
+                    physical_sector_size: info.physical_sector_size,
+                },
+            );
+
+            match info.variant {
+                DeviceVariant::Loopback(backing_file) => {
+                    self.storage.loopbacks.insert(whole_entity, backing_file);
+                }
+                DeviceVariant::Map(devmapper) => {
+                    self.storage.device_maps.insert(whole_entity, devmapper);
+                }
+                DeviceVariant::Physical(table) => {
+                    self.storage.disks.insert(whole_entity, Disk { serial: String::new(), table });
+                }
+            }
+
+            if let Some(fstype) = info.fstype {
+                self.storage.partitions.insert(
+                    whole_entity,
+                    Partition {
+                        offset:      0,
+                        number:      0,
+                        filesystem:  fstype.parse().ok(),
+                        partuuid:    None,
+                        partlabel:   None,
+                        mbr_variant: PartitionType::Primary,
+                        uuid:        info.uuid,
+                    },
+                );
+            }
+
+            let mut children = Vec::new();
+            for partition in info.partitions {
+                let part_entity = self.entities.insert(());
+                children.push(part_entity);
+
+                self.storage.parents.insert(part_entity, vec![whole_entity]);
+
+                self.storage.devices.insert(
+                    part_entity,
+                    Device {
+                        name:                 partition.device,
+                        path:                 partition.path,
+                        sectors:              partition.sectors,
+                        logical_sector_size:  info.logical_sector_size,
+                        physical_sector_size: info.physical_sector_size,
+                    },
+                );
+
+                self.storage.partitions.insert(
+                    part_entity,
+                    Partition {
+                        offset:      partition.offset,
+                        number:      partition.no,
+                        filesystem:  partition.fstype.and_then(|fstype| fstype.parse().ok()),
+                        partuuid:    partition.partuuid,
+                        partlabel:   partition.partlabel,
+                        mbr_variant: PartitionType::Primary,
+                        uuid:        partition.uuid,
+                    },
+                );
+            }
+
+            self.storage.children.insert(whole_entity, children);
+        }
+
+        self.associate_slaves();
+
+        Ok(())
+    }
+
+    #[cfg(not(target_os = "linux"))]
+    pub fn scan_(&mut self) -> Result<(), DiskError> {
+        compile_error!("Only Linux is supported at the moment");
+    }
+
+    fn associate_slaves(&mut self) {
+        let devices = &self.storage.devices;
+        let parents = &mut self.storage.parents;
+
+        for (entity, device) in devices {
+            for slave in slaves_iter(&device.name) {
+                for (other_entity, other_device) in devices {
+                    if other_device.name == slave {
+                        match parents.get_mut(entity) {
+                            Some(associations) => associations.push(other_entity),
+                            None => drop(parents.insert(entity, vec![other_entity])),
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 #[derive(Debug, Default)]
@@ -158,26 +269,33 @@ pub struct DeviceEntity<'a> {
 }
 
 impl<'a> DeviceEntity<'a> {
-    // Returns the child of this device, if there is a child.
-    pub fn child<'b>(&'b self) -> Option<DeviceEntity<'b>> {
-        self.ctx.storage.children.get(self.id).map(move |&id| DeviceEntity { id, ctx: self.ctx })
+    // If the device is a loopback, this will kdisplay the backing file.
+    pub fn backing_file<'b>(&'b self) -> Option<&'a Path> {
+        self.ctx.storage.loopbacks.get(self.id).map(AsRef::as_ref)
+    }
+
+    // Provides an iterator for listing children of a device, for devices that support having
+    // multiple children.
+    pub fn children<'b>(&'b self) -> impl Iterator<Item = DeviceEntity<'b>> {
+        self.ctx
+            .storage
+            .children
+            .get(self.id)
+            .into_iter()
+            .flat_map(|entities| entities.iter())
+            .map(move |&id| DeviceEntity { id, ctx: self.ctx })
     }
 
     // Access information about this device.
     pub fn device<'b>(&'b self) -> &'b Device { self.ctx.device(self.id) }
 
+    // If the device is a device map, this will return its name.
+    pub fn device_map_name<'b>(&'b self) -> Option<&'b str> {
+        self.ctx.storage.device_maps.get(self.id).map(AsRef::as_ref)
+    }
+
     // If the device is a disk, information about that disk can be retrieved here.
     pub fn disk<'b>(&'b self) -> Option<&'b Disk> { self.ctx.storage.disks.get(self.id) }
-
-    #[auto_enum(Iterator)]
-    pub fn table<'b>(&'b self) -> impl Iterator<Item = DeviceEntity<'b>> {
-        match self.ctx.storage.tables.get(self.id) {
-            None => iter::empty(),
-            Some(entities) => {
-                entities.iter().cloned().map(move |id| DeviceEntity { id, ctx: self.ctx })
-            }
-        }
-    }
 
     // If the device is part of a LVM group, information about the LVM device is here.
     pub fn lvm<'b>(&'b self) -> Option<&'b Lvm> { self.ctx.storage.lvms.get(self.id) }
@@ -186,7 +304,17 @@ impl<'a> DeviceEntity<'a> {
     pub fn luks<'b>(&'b self) -> Option<&'b Luks> { self.ctx.storage.luks.get(self.id) }
 
     // Return the parent of this device, if this device has one.
-    pub fn parent<'b>(&'b self) -> Option<DeviceEntity<'b>> {
-        self.ctx.storage.parents.get(self.id).map(move |&id| DeviceEntity { id, ctx: self.ctx })
+    pub fn parents<'b>(&'b self) -> impl Iterator<Item = DeviceEntity<'b>> {
+        self.ctx
+            .storage
+            .parents
+            .get(self.id)
+            .into_iter()
+            .flat_map(|entities| entities.iter())
+            .map(move |&id| DeviceEntity { id, ctx: self.ctx })
+    }
+
+    pub fn partition<'b>(&'b self) -> Option<&'a Partition> {
+        self.ctx.storage.partitions.get(self.id)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,21 @@ fn main() {
 
     for entity in manager.devices() {
         let device = entity.device();
-        println!("{}", device.path.display());
-        for entity in entity.table() {
-            let device = entity.device();
-            println!("├─{}:", device.path.display(),);
+        println!("{}", device.name);
+
+        if let Some(dm_name) = entity.device_map_name() {
+            println!("  dm_name: {}", dm_name);
+        } else if let Some(backing_file) = entity.backing_file() {
+            println!("  backing_file: {}", backing_file.display());
+        } else if let Some(table) = entity.disk() {
+            println!("  table: {}", <&'static str>::from(table));
+        }
+
+        for child in entity.children() {
+            println!("  child: {}", child.device().name);
+        }
+        for parent in entity.parents() {
+            println!("  parent: {}", parent.device().name);
         }
     }
 }
